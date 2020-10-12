163e2a2b695f888f57557d49c781ed2c83029b05
XlhsirgSieciB <ewuuw632vmv4515@126.com>
2020-10-12 20:34:24 +0800

    pom test

--- src/main/java/com/fm/mirror/util/ClassPropertyUtil.java
+++ src/main/java/com/fm/mirror/util/ClassPropertyUtil.java
@@ -1,6 +1,7 @@
 package com.fm.mirror.util;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 public class ClassPropertyUtil {
 	@SuppressWarnings("unchecked")
@@ -14,14 +15,19 @@ public class ClassPropertyUtil {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	public static void SetProperty(Object obj, String name, Object value) {
+	public static void setProperty(Object obj, String name, Object value) {
 		try {
 			Field field = obj.getClass().getDeclaredField(name);
 			field.setAccessible(true);
 			field.set(obj, value);
 		} catch (NoSuchFieldException | IllegalAccessException e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	public static void setProperties(Object obj, Map<String, ? extends Object> properties) {
+		for (Map.Entry<String, ? extends Object> entry : properties.entrySet()) {
+			setProperty(obj, entry.getKey(), entry.getValue());
 		}
 	}
 }
