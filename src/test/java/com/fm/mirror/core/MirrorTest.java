2fca009b3583373025ab7d0c52c2ac3e28180a67
XlhsirgSieciB <ewuuw632vmv4515@126.com>
2021-01-29 13:22:17 +0800

    log

--- src/test/java/com/fm/mirror/core/MirrorTest.java
+++ src/test/java/com/fm/mirror/core/MirrorTest.java
@@ -1,4 +1,16 @@
 package com.fm.mirror.core;
 
+import org.junit.Test;
+
+import java.util.Map;
+
 public class MirrorTest {
+	@Test
+	public void getInstanceTest() {
+		Map<String, Object> map1 = Mirror.getInstance("Map");
+		assert map1 != null;
+		Map<String, Object> map2 = Mirror.getInstance("Map");
+		assert map2 != null;
+		assert map1 != map2;
+	}
 }
